399dc35ce13818a9b8cb29e02627af8792c46f8a
Noor <thenoorulhassan.dev@gmail.com>
2026-07-01 01:48:52 +0500

    Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+export default function App() {
   return (
-    <>
-      <header className="flex flex-col gap-1">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </header>
-      <h1 className="bg-black">Hi </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
