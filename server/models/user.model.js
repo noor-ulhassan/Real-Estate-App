e6bd0838f5231cbdb01105a9f765e0b127d5f4b5
Noor <thenoorulhassan.dev@gmail.com>
2026-07-01 03:22:15 +0500

    user model update

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     email: {
+      type: String,
+      required: [true, "Email is Required for creating a user"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Invalid E-mail Address",
+      ],
+    },
+    username: {
       type: String,
       required: true,
       unique: true,
@@ -15,6 +22,8 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      minLength: [6, "Password should contain at-least 6 characters"],
+      select: false,
     },
   },
   {
@@ -22,6 +31,17 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-userSchema.pre();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  return;
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
