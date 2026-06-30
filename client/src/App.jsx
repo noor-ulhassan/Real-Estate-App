0bc68bdf57462355f9c5cde0183f75ab81f54566
Noor <thenoorulhassan.dev@gmail.com>
2026-07-01 01:11:13 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <header className="flex flex-col gap-1">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+      <h1 className="bg-black">Hi </h1>
+    </>
+  );
+}
+
+export default App;
