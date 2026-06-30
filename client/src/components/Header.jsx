02d0452a90aa3b96bb07b7f0abf8ff578c016313
Noor <thenoorulhassan.dev@gmail.com>
2026-07-01 02:42:11 +0500

    Header Section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <>
+      <header className=" bg-slate-200 shadow-md ">
+        <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
+          <Link to="/">
+            <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+              <span className="text-slate-500">Noor</span>
+              <span className="text-slate-700">Estate</span>
+            </h1>
+          </Link>
+          <form className="bg-slate-100 rounded-lg flex items-center p-2">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent focus:outline-none w-24 sm:w-64"
+            />
+            <FaSearch className="text-slate-600 cursor-pointer" />
+          </form>
+          <ul className="flex gap-4 text-slate-700 text-sm font-semibold">
+            <Link to="/">
+              <li className="hidden sm:inline hover:text-slate-400">Home</li>
+            </Link>
+            <Link to="/about">
+              <li className="hover:text-slate-400 hidden sm:inline">About</li>
+            </Link>
+            <Link to="/sign-in">
+              <li className="hover:text-slate-400 ">Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
